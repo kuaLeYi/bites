0f3c77939230ab42818d23a14a342d7bdf63c5d2
kuaLeYi <23028142+kuaLeYi@users.noreply.github.com>
2021-04-20 10:57:02 +0800

    Some doc for puzzle-lite/

--- puzzle-lite/index.js
+++ puzzle-lite/index.js
@@ -1,9 +1,19 @@
+// Disp new game depending on level i
 function nuG(i) {
   var obj={};
+  // Visually refresh level, indicate current
   $(".btnlvlcurr").removeClass("btnlvlcurr");
   $("#btn"+i).addClass("btnlvlcurr");
   $("#imgct").addClass("hid");
   $("#cvs").removeClass("hid");
+  /* Set important values for the level's puzzle picture,
+    currently all hardcoded:
+    .siz is size in grid units
+    .excIntArr is integer array of grid positions excluded
+      because appear completely empty, indistinguishable
+    .dnbl is art ID at source http://dan-ball.jp
+    .fracX, .fracY are dimensions per 'puzzle piece'
+    .cvsW, .cvsH are canvas dimensions */
   switch(i){
     case 1:
       obj.siz=6;
@@ -30,7 +40,10 @@ function nuG(i) {
   }
   baseNuG(obj);
 }
+/* Generate puzzle to be displayed using nuG() given
+  properties (see above) */
 function baseNuG(obj) {
+  // Durstenfeld shuffle, to apply to 'puzzle piece' array
   function sA(x) {
     for (let i=x.length-1; i>0; i--) {
       const j = ~~(Math.random()*(i+1));
@@ -39,22 +52,26 @@ function baseNuG(obj) {
   }
   var img=new Image(),
     x,y,size=obj.siz,
+    // Nested array of row and column coordinates for every grid unit
     n=[...Array(size**2)].map((e,i)=>[i/size|0,i%size]),
     exc=obj.excIntArr,
     cou=0,
-    saneScor= size**2-obj.excIntArr.length;
+    saneScor=size**2-obj.excIntArr.length;
   img.id=obj.picId;
   img.src=obj.filnam;
   $("#imgctlk").empty();
   imgctlk.appendChild(img);
   imgctlk.href=`http://dan-ball.jp/en/javagame/dust2/${obj.dnbl}.html`;
   [x,y]=[obj.fracX,obj.fracY];
+  // Shuffle grid coordinates; will be compared against sorted during play
   sA(n);
   cvs.width=obj.cvsW;
   cvs.height=obj.cvsH;
+  // Check 'piece' positions, except exc, against correct ones
   function isSolved() {
     return n.map((e,i)=>e[0]*size+e[1]==i).filter((e,i)=>exc.indexOf(i)<0).reduce((a,b)=>a+b,0)==saneScor;
   }
+  // Add functionality to image only when it's loaded
   img.onload=function() {
     var ctx = cvs.getContext("2d"), i,j, oIdeX, oIdeY;
     function drawPiece(a,b,c,d) {
@@ -66,6 +83,9 @@ function baseNuG(obj) {
         drawPiece(a[0],a[1],i,j);
       }
     }
+    /* Swop 'pieces' on mouse/finger event. Player
+      clicks first 'piece', then its destination;
+      they swop as follows */
     function mov(e) {
       var ex=e.pageX-(cvs.offsetLeft+cvs.clientLeft),
         ey=e.pageY-(cvs.offsetTop+cvs.clientTop),
@@ -79,6 +99,7 @@ function baseNuG(obj) {
         [n[a],n[b]]=[n[b],n[a]];
         drawPiece(n[a][0],n[a][1],ideX,ideY);
         drawPiece(n[b][0],n[b][1],oIdeX,oIdeY);
+        // Handle solved state: currently must stop listening for events
         if(isSolved()){
           remHandler();
         }
@@ -87,6 +108,7 @@ function baseNuG(obj) {
     cvs.addEventListener("click", mov);
     function remHandler() {
       cvs.removeEventListener("click", mov);
+      // Wait 3000 ms, replace solved grid by the image w/o grid lines
       setTimeout(function() {
         $("#cvs").addClass("hid");
         $("#imgct").removeClass("hid");
@@ -96,4 +118,4 @@ function baseNuG(obj) {
     }
   }
 }
-nuG(1);
+nuG(1); // Run level 1 by default for player's convenience
